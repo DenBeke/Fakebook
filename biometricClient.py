d55fa02c3a30f17ce218d582bc9f98b37416f354
verachtertr <verachtertr@gmail.com>
2016-05-10 15:05:33 +0200

    Biometric client reads automatically the data, and pushes it to the websocket

--- biometricClient.py
+++ biometricClient.py
@@ -1,5 +1,6 @@
 from websocket import create_connection
 from datetime import datetime
+import sys
 """
 Takes a json String and looks for the date field and changes it to the current date.
 """
@@ -7,30 +8,29 @@ def changeToCorrectDate(jsonString):
 	date = datetime.today()
 #	yearString = date.year + "-" + date.month + "-" + date.day
 	yearString = date.strftime("%Y-%m-%d")
-	print yearString
 	pos = jsonString.find("2015-01-01")
-	print pos
 	jsonString = jsonString.replace("2015-01-01", yearString)
 	return jsonString
 	
 def setUser(jsonString,userId):
 	pos = jsonString.find("joe")
-	print pos
 	jsonString = jsonString.replace("\"joe\"", str(userId))
-	print jsonString
 	return jsonString
 
 if __name__ == "__main__":
 	
-	with open('data/example.json', 'r') as bioData:
-		json = bioData.read()
-
+	with open('data/heart-rate.json', 'r') as bioData:
 		ws = create_connection("ws://localhost:8080/Fakebook/BiometricDataEndpoint")
-		print "Sending example.json "
-		print changeToCorrectDate(json)
-		data = changeToCorrectDate(json)
-		data = setUser(data, 1)
-		print data
-		ws.send(data)
-		print "Sent"
+		realTime = False
+		if len(sys.argv) > 1:
+			realTime = (sys.argv[1]=="--realTime")
+
+		print "Started sending data."
+		if not realTime:
+			for line in bioData.readlines():
+
+				data = changeToCorrectDate(line)
+				data = setUser(data, 1)
+				ws.send(data)
+		print "Sent all data."
 		ws.close()
