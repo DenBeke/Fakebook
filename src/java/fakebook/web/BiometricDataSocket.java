849df9db467964f921340d6a9f19f727a5527e1b
verachtertr <verachtertr@gmail.com>
2016-05-16 18:07:46 +0200

    Refactored BiometricDataSocket  to be in web package

--- src/java/fakebook/web/BiometricDataSocket.java
+++ src/java/fakebook/web/BiometricDataSocket.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fakebook.representation;
+package fakebook.web;
 
 import fakebook.business.BiometricServiceFacadeLocal;
 import java.io.StringReader;
